6cb4fef5dd13730927563313dad7645e823e7ef4
its_florent <70605487+its-Florent@users.noreply.github.com>
2021-01-09 21:47:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext \
+    import commands
+import asyncio
+import os
+from discord.ext import commands
+
+extensions = ['']# Your Cog
+
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)# Cog Load
+        except Exception as error:
+            print('{} konnte nicht geladen werden. [{}]'.format(extension, error))
+
+
+@bot.event  # Ready Event
+async def on_ready():
+    print("Bot Online mit:")
+    print("Username: ", bot.user.name)
+    print("User ID: ", bot.user.id)
+
+
+bot.run("TOKEN")
